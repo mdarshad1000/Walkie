af937ed672d34850a771ef307b4672e4ff12a6f9
Mohammad Arshad <mdarshad1000@gmail.com>
2022-09-30 23:24:41 +0530

    Add Database models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants = 
+    # Takes a snapshot everytime we save
+    updated = models.DateTimeField(auto_now=True)
+    # Takes a snapshot only when created
+    created = models.DateTimeField(auto_now_add=True)
