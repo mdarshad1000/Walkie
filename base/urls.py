196fd39e8426ecdbfb3facd2c6865f967470c3e7
Mohammad Arshad <mdarshad1000@gmail.com>
2022-10-02 01:35:49 +0530

    Add Login

--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     # good practice to name it too and is convinient for accessing in templates.
+    path('login/', views.loginPage, name='login'), 
+    
     path('', views.home, name='home'), 
     path('room/<str:pk>', views.room, name='room'),
+
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>', views.deleteRoom, name='delete-room')
--- base/views.py
+++ base/views.py
@@ -1,20 +1,49 @@
-from multiprocessing import context
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
-rooms = [
-    {'id':1, 'name':'Lets learn Flask!'},
-    {'id':2, 'name':'100 Days of Code'},
-    {'id':3, 'name':'CI CD in one shot'},
-]
+# rooms = [
+#     {'id':1, 'name':'Lets learn Flask!'},
+#     {'id':2, 'name':'100 Days of Code'},
+#     {'id':3, 'name':'CI CD in one shot'},
+# ]
+
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist!')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 
 def home(request):
-    # Querying the Room
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    # Querying the Room if query matches or contains the query entered.
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    '''
+    Q --> so that we can serach not just by topic, but by name, description etc
+    | --> stands for OR
+    '''
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
+                                Q(name__icontains=q) |
+                                Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count':room_count}
     return render(request, 'base/home.html', context)  # {'how to address in template':what to pass}
     
 
