d71088b6abe2ec3d097eed28b3423189c2019644
Mohammad Arshad <mdarshad1000@gmail.com>
2022-10-04 03:56:21 +0530

    update api

--- base/api/views.py
+++ base/api/views.py
@@ -1,8 +1,9 @@
 from django.http import JsonResponse
 
+
 def getRoute(request):
     routes = [
         'GET /api/rooms',
         'GET /api/rooms/:id' 
     ]
-    return JsonResponse()
+    return JsonResponse(routes, safe=False)
