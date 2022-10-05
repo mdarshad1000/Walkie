a84a696ec8a4f2055c27932d7a3098e7e62259f0
Mohammad Arshad <mdarshad1000@gmail.com>
2022-10-05 20:47:57 +0530

    add customUserModel

--- base/models.py
+++ base/models.py
@@ -1,10 +1,19 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    # Use email instead of username
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
 class Topic(models.Model):
